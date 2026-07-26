ffc8f53514bf83166f293571b5ac8f528a549ac3
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-25 23:03:22 -0400

    expose mutations through the actions package

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,1 +1,14 @@
-export { BUILTIN_ACTORS, actorApplicable, runBrowserActor } from "../lib/actors.js";
+export { applyOperation, operation, saveDocumentBatch } from "../lib/operations.js";
+export {
+  BUILTIN_ACTORS,
+  actorApplicable,
+  actorApplicability,
+  isBuiltinActor,
+  runBrowserActor
+} from "../lib/actors.js";
+export {
+  ACTOR_TRANSFORM_OPERATIONS,
+  actorWithTransformEnvelope,
+  buildActorTransform,
+  normalizeActorTransformResult
+} from "../lib/actor-transforms.js";
