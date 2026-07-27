190f45cfb1e34193bde35ed47a1dd219ee92363e
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-27 03:14:16 -0400

    load agent chat styles

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -22,6 +22,7 @@ import "../mobile-graph-tools.css";
 import "../mobile-graph-empty-state.css";
 import "../graph-editors.css";
 import "../graph-editors-extra.css";
+import "../agent-chat.css";
 
 initializeTheme();
 
