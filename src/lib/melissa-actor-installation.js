269334cdd04b2ef29c69958ff5dcab9dd364938f
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-29 09:15:09 -0400

    fix: centralize Melissa runtime pack installation

---
+++ src/lib/melissa-actor-installation.js
@@ -0,0 +1,15 @@
+import {
+  MELISSA_ACTOR_PACK_VERSION,
+  mergeMelissaActors
+} from "./melissa-actor-pack-runtime";
+
+export function installMelissaActorPack(settings = {}) {
+  return {
+    actors: mergeMelissaActors(settings.actors || []),
+    actorsEnabled: true,
+    melissaActorPackInstalled: true,
+    melissaActorPackVersion: MELISSA_ACTOR_PACK_VERSION
+  };
+}
+
+export { MELISSA_ACTOR_PACK_VERSION };
