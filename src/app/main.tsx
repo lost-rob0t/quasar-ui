34944c28b19d872f23022c4c8f59a69d63dac9eb
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-08 21:00:52 -0400

    Load the mirrored black and gold shell

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -32,6 +32,7 @@ import "../graph-workspace-shell.css";
 import "../graph-full-viewport-modern.css";
 import "../melissa-actors.css";
 import "../actor-configuration.css";
+import "../kinpaku-shell.css";
 
 initializeTheme();
 
