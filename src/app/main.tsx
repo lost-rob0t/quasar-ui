014c92cf6a67084cfc6021d1024befbc62a239f4
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-19 00:17:45 -0400

    Stop loading the legacy graph shell override layer

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -24,7 +24,6 @@ import "../dashboard-theme.css";
 import "../mobile.css";
 import "../mobile-editor.css";
 import "../gesture-menu.css";
-import "../operator-ui.css";
 import "../dataset-menu.css";
 import "../mobile-graph-tools.css";
 import "../mobile-graph-empty-state.css";
